7c0dcd8aedf6463945b076eebca7d0ae900a2883
Yong Wen Chua <me@yongwen.xyz>
2018-01-25 11:32:19 +0800

    Add eslint

--- contracts/validator/migrations/2_validators.js
+++ contracts/validator/migrations/2_validators.js
@@ -1,12 +1,3 @@
-// var AddressVotes = artifacts.require("./libraries/AddressVotes.sol");
-// var MajoritySet = artifacts.require("./MajoritySet.sol")
-
-// module.exports = function (deployer) {
-//     deployer.deploy(AddressVotes);
-//     deployer.link(AddressVotes, [MajoritySet]);
-//     deployer.deploy(MajoritySet);
-// };
-
 const OuterSet = artifacts.require("./OuterSet.sol");
 const InnerSetInitial = artifacts.require("./InnerSetInitial.sol");
 const InnerMajortiySet = artifacts.require("./InnerMajoritySet");
@@ -14,35 +5,38 @@ const AddressVotes = artifacts.require("./libraries/AddressVotes.sol");
 
 const genesisOuterSetAddress = "0x0000000000000000000000000000000000000005";
 
-const networkDeployers = {
-    "development": deployDevelopment,
-    "parity_master": deployParity,
-    "parity_authority1": (deployer) => {},
-    "parity_authority2": (deployer) => {},
-    "parity_authority3": (deployer) => {},
-};
-
 function deployInnerMajoritySet(deployer, outerSetAddress) {
-    deployer.deploy(AddressVotes);
-    deployer.link(AddressVotes, InnerMajortiySet);
+  deployer.deploy(AddressVotes);
+  deployer.link(AddressVotes, InnerMajortiySet);
 
-    return deployer.deploy(InnerMajortiySet, outerSetAddress)
-        .then(() => OuterSet.at(outerSetAddress))
-        .then(instance => instance.setInner(InnerMajortiySet.address));
+  return deployer
+    .deploy(InnerMajortiySet, outerSetAddress)
+    .then(() => OuterSet.at(outerSetAddress))
+    .then(instance => instance.setInner(InnerMajortiySet.address));
 }
 
 // In a development environment, we need to deploy `OuterSet` manually.
 // `InnerSetInitial` can be skipped
 function deployDevelopment(deployer) {
-    deployer.deploy(InnerSetInitial).then(() => deployer.deploy(OuterSet, InnerSetInitial.address));
+  deployer
+    .deploy(InnerSetInitial)
+    .then(() => deployer.deploy(OuterSet, InnerSetInitial.address));
 
-    return deployInnerMajoritySet(deployer, OuterSet.address);
+  return deployInnerMajoritySet(deployer, OuterSet.address);
 }
 
 function deployParity(deployer) {
-    return deployInnerMajoritySet(deployer, genesisOuterSetAddress);
+  return deployInnerMajoritySet(deployer, genesisOuterSetAddress);
 }
 
-module.exports = function (deployer, network) {
-    networkDeployers[network](deployer);
+const networkDeployers = {
+  development: deployDevelopment,
+  parity_master: deployParity,
+  parity_authority1: () => {},
+  parity_authority2: () => {},
+  parity_authority3: () => {}
+};
+
+module.exports = (deployer, network) => {
+  networkDeployers[network](deployer);
 };
