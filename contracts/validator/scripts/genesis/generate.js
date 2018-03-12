8de1e07ad36a5bd50884adf78451294778374077
Ng Guoyou <gyng@users.noreply.github.com>
2018-03-12 12:43:17 +0800

    Add script to generate genesis block JSON (#7)
    * Add initial CLI tool to generate genesis JSON definition
    
    * Move genesis script into directory
    
    * Add basic documentation on how to run the genesis generator script
    
    * Combine validatorsContract option with outer option in genesis script
    
    * Add units to blockReward and stepDuration help strings
    
    * Remove validatorContract from generate.js readme

---
+++ contracts/validator/scripts/genesis/generate.js
@@ -0,0 +1,78 @@
+const yargs = require("yargs");
+const _ = require("lodash");
+const template = require("./genesisTemplate.json");
+
+const OuterSet = artifacts.require("OuterSet");
+const InnerSetInitial = artifacts.require("InnerSetInitial");
+
+module.exports = async function generateGenesis(cb) {
+  try {
+    const { argv } = yargs
+      .usage(
+        [
+          "Use Truffle to generate a genesis JSON file for a Parity PoA network",
+          "",
+          "truffle exec scripts/genesis/generate.js -v <addr> -v <addr> --master=<addr> --outer=<addr> --inner=<addr> --validatorsContract=<addr>"
+        ].join("\n")
+      )
+      .version("0.0.1")
+      .option("master", {
+        demand: true,
+        describe: "address for master account"
+      })
+      .option("validator", {
+        alias: "v",
+        describe:
+          "(multiple) addresses of initial validators, repeat option for multiple"
+      })
+      .option("name", { describe: "network name" })
+      .option("blockReward", { describe: "in Wei" })
+      .option("stepDuration", { describe: "voting round length in seconds" })
+      .option("outer", { demand: true, describe: "outer set contract address" })
+      .option("inner", {
+        demand: true,
+        describe: "initial inner set contract address"
+      })
+      .option("networkId", { describe: "network ID" });
+
+    const outer = await OuterSet.new(0, argv.master);
+    const initial = await InnerSetInitial.new(0, [].concat(argv.validator));
+
+    const accounts = {};
+    accounts[argv.outer] = {
+      balance: 1,
+      constructor: outer.constructor.bytecode
+    };
+    accounts[argv.inner] = {
+      balance: 1,
+      constructor: initial.constructor.bytecode
+    };
+
+    const options = {
+      name: argv.name,
+      engine: {
+        authorityRound: {
+          params: {
+            stepDuration: argv.stepDuration,
+            validators: {
+              contract: argv.outer
+            },
+            blockReward: argv.blockReward
+          }
+        }
+      },
+      params: {
+        networkID: argv.networkID
+      },
+      accounts
+    };
+
+    // Deep merge
+    const result = _.merge({}, template, options);
+    console.log(JSON.stringify(result, null, 2)); // eslint-disable-line
+  } catch (e) {
+    cb(e);
+    return;
+  }
+  cb();
+};
